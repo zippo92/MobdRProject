2c662d76f889b32651bfe9c3dc5b6deb7a642452
simone <simone.zippo@gmail.com>
2018-02-19 17:45:19 +0100

    data splitting

--- Main.R
+++ Main.R
@@ -4,11 +4,15 @@ library("LiblineaR")
 library("e1071")
 source("data_understanding.R")
 source("data_preparation.R")
-source("data_splitting.R")
+source("data_splitting_nFold.R")
 source("data_normalization.R")
 
 fullSet <- read.csv("training_R.csv",row.names=1, sep=";")
 
 data_summary <- data_understanding(fullSet);
-my_data <- data_preparation(fullSet);
-data_summary <- data_understanding(my_data);
+cleaned_data <- data_preparation(fullSet);
+#data_summary <- data_understanding(my_data);
+
+splitted_data <- split(cleaned_data,5);
+#splitted_data contiene N split, ognuno diviso in train e test 
+#Ex: splitted_data$split_2$train
---
+++ data_splitting_nFold.R
@@ -0,0 +1,54 @@
+#IL KFOLD consiste nel dividere le istanze in K split, e iterativamente una viene utilizzata per il validation/test
+# e le altre per il training
+
+split <- function(my_data, K){
+  set.seed(123)
+  nFoldSplit <- c(); 
+  
+  #Per avere lo stesso bilanciamento di istanze positive e negative, prendo la stessa percentuale in ogni split
+  output <- by(my_data, as.factor(cleaned_data$shares),function(x){
+  smp_size <- floor(nrow(x)/K);
+  print(smp_size)
+  splitted_data <- list(); 
+  #creo K segmenti differenti, della stessa dimensione (smp_size)
+  for(i in 1:K){
+      split_ind <- sample(seq_len(nrow(x)), size = smp_size)
+      split <- x[split_ind, ]
+      x<-x[-split_ind,];  
+      splitted_data[[length(splitted_data)+1]] <- split
+  }
+  return(splitted_data)
+  })
+  
+  
+  splitted_data<-list();
+  
+  #Per ogni split, innanzitutto unisco le istanze positive dalle negative, poi scelgo un segmento di test e gli altri
+  #saranno di training. 
+  for(i in 1:K){
+    test_set <- rbind(output[[1]][[i]], output[[2]][[i]]); 
+    training_set <- data.frame();
+    for(j in 1:K)
+    {
+      if(i != j)
+      {
+        training_set <- rbind(training_set,output[[1]][[j]], output[[2]][[j]]);
+      }
+    }
+  
+    #shuffle row-wise
+    training_set <- training_set[sample(nrow(training_set)),]
+    test_set <- test_set[sample(nrow(test_set)),]
+    
+    
+    traintest <- list(training_set,test_set)
+    names(traintest) <- c("train", "test"); 
+    
+    splitted_data[[length(splitted_data)+1]] <- traintest
+    names(splitted_data)[i] <-paste("split_",i, sep="");
+      
+  }
+return(splitted_data)
+
+}
+
